cf33e5105aaa90338a2716f6fd1dbcd84a65f405
Vladimir Bajceta <bajceta@gmail.com>
2016-11-10 10:14:07 +0100

    updated config for t530

--- keyboard.sh
+++ keyboard.sh
@@ -1,2 +1,3 @@
-setxkbmap -option grp:alt_shift_toggle -option ctrl:nocaps us,se,rs
+setxkbmap -option grp:alt_shift_toggle -option ctrl:nocaps -layout us,se,rs -variant ",,latin"
+# setxkbmap -option grp:alt_shift_toggle -option ctrl:nocaps us,se,rs
 
